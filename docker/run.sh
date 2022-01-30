ad9ce0c2ebe085c83eb651ff9265fc8abb416b73
JamesPerlman <jam.e.perl@gmail.com>
2022-01-30 12:52:35 -0800

    remove x11 stuff from docker run file

--- docker/run.sh
+++ docker/run.sh
@@ -29,11 +29,4 @@ docker run \
     --gpus=all \
     -v "${INPUT_PATH}":"${WORKDIR}/content/input" \
     -v "${OUTPUT_PATH}":"${WORKDIR}/content/output" \
-    -v /usr/lib/wsl:/usr/lib/wsl \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /mnt/wslg:/mnt/wslg \
-    -e DISPLAY=$DISPLAY \
-    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
-    -e LIBGL_ALWAYS_SOFTWARE=1 \
-    --device=/dev/dxg \
     video2nerfie
