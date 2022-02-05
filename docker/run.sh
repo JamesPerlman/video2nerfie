72c057b09e6ae67562702be70212d1b93d7906ce
JamesPerlman <jam.e.perl@gmail.com>
2022-02-04 20:35:29 -0800

    update Dockerfile and run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-while getopts ":i:o:" opt; do
+while getopts ":i:" opt; do
     case $opt in
         i)
             INPUT_PATH=$OPTARG
-            ;;
-        o)
-            OUTPUT_PATH=$OPTARG
             ;;
     esac
 done
@@ -16,17 +13,11 @@ if [ -z "${INPUT_PATH}" ]; then
     exit 1
 fi
 
-if [ -z "${OUTPUT_PATH}" ]; then
-    echo "Missing output path for -o argument"
-    exit 1
-fi
-
 WORKDIR=/usr/local/video2nerfie
 
 docker run \
     -it \
     --rm \
     --gpus=all \
-    -v "${INPUT_PATH}":"${WORKDIR}/content/input" \
-    -v "${OUTPUT_PATH}":"${WORKDIR}/content/output" \
+    -v "${INPUT_PATH}":"${WORKDIR}/content" \
     video2nerfie
