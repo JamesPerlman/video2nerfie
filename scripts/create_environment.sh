ddbf9bcef5a0bc1d1f6b5055d757daff959aac68
JamesPerlman <jam.e.perl@gmail.com>
2022-02-08 18:18:49 -0800

    non-interactive pip install

--- scripts/create_environment.sh
+++ scripts/create_environment.sh
@@ -2,6 +2,6 @@
 conda create -n video2nerfie python=3.8
 source activate video2nerfie
 
-pip install https://storage.googleapis.com/jax-releases/cuda11/jaxlib-0.1.75+cuda11.cudnn82-cp38-none-manylinux2010_x86_64.whl
+pip install -y https://storage.googleapis.com/jax-releases/cuda11/jaxlib-0.1.75+cuda11.cudnn82-cp38-none-manylinux2010_x86_64.whl
 
 pip install -r requirements.txt
