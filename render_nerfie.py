0358e8748fdf2bef82271e46a69e8b16fb96a3c4
JamesPerlman <jam.e.perl@gmail.com>
2022-02-14 21:47:13 -0800

    formatting\!

--- render_nerfie.py
+++ render_nerfie.py
@@ -1,4 +1,34 @@
 # ARGS
+from nerfies import utils
+from nerfies import visualization as viz
+from nerfies import evaluation
+import functools
+from nerfies import training
+from nerfies import schedules
+from nerfies import model_utils
+from nerfies import models
+from nerfies import image_utils
+from nerfies import datasets
+from nerfies import configs
+import gin
+import mediapy
+import imageio
+import tempfile
+import numpy as np
+import random as pyrandom
+from io import BytesIO
+from absl import logging
+from flax.training import checkpoints
+from flax.metrics import tensorboard
+from flax import optim
+from flax import jax_utils
+import flax.linen as nn
+import flax
+from jax import random
+from jax import grad, jit, vmap
+import jax.numpy as jnp
+from jax.config import config as jax_config
+import jax
 from pathlib import Path
 
 project_dir = Path('/mnt/e/2022/NeRF/Nerfies/cortines')
@@ -9,44 +39,25 @@ camera_dir = data_dir / 'camera-paths/orbit-mild'
 render_dir = project_dir / 'render-default-nerfies-cameras'
 
 # @title Configure notebook runtime
-# @markdown If you would like to use a GPU runtime instead, change the runtime type by going to `Runtime > Change runtime type`. 
+# @markdown If you would like to use a GPU runtime instead, change the runtime type by going to `Runtime > Change runtime type`.
 # @markdown You will have to use a smaller batch size on GPU.
-import jax
 runtime_type = 'gpu'  # @param ['gpu', 'tpu']
 if runtime_type == 'tpu':
-  import jax.tools.colab_tpu
-  jax.tools.colab_tpu.setup_tpu()
+    import jax.tools.colab_tpu
+    jax.tools.colab_tpu.setup_tpu()
 
 print('Detected Devices:', jax.devices())
 
 # @title Define imports and utility functions.
 
-import jax
-from jax.config import config as jax_config
-import jax.numpy as jnp
-from jax import grad, jit, vmap
-from jax import random
-
-import flax
-import flax.linen as nn
-from flax import jax_utils
-from flax import optim
-from flax.metrics import tensorboard
-from flax.training import checkpoints
-
-from absl import logging
-from io import BytesIO
-import random as pyrandom
-import numpy as np
-import tempfile
-import imageio
-import mediapy
 
 # Monkey patch logging.
-def myprint(msg, *args, **kwargs):
- print(msg % args)
 
-logging.info = myprint 
+def myprint(msg, *args, **kwargs):
+    print(msg % args)
+
+
+logging.info = myprint
 logging.warn = myprint
 logging.error = myprint
 
@@ -54,23 +65,19 @@ logging.error = myprint
 # @markdown Change the directories to where you saved your capture and experiment.
 
 
-import gin
-from nerfies import configs
-
-
 checkpoint_dir = train_dir / 'checkpoints'
 checkpoint_dir.mkdir(exist_ok=True, parents=True)
 
 config_path = train_dir / 'config.gin'
 with open(config_path, 'r') as f:
-  logging.info('Loading config from %s', config_path)
-  config_str = f.read()
+    logging.info('Loading config from %s', config_path)
+    config_str = f.read()
 gin.parse_config(config_str)
 
 config_path = Path(train_dir, 'config.gin')
 with open(config_path, 'w') as f:
-  logging.info('Saving config to %s', config_path)
-  f.write(config_str)
+    logging.info('Saving config to %s', config_path)
+    f.write(config_str)
 
 exp_config = configs.ExperimentConfig()
 model_config = configs.ModelConfig()
@@ -80,26 +87,19 @@ eval_config = configs.EvalConfig()
 
 # @title Create datasource and show an example.
 
-from nerfies import datasets
-from nerfies import image_utils
 
 datasource = datasets.from_config(
-  exp_config.datasource_spec,
-  image_scale=exp_config.image_scale,
-  use_appearance_id=model_config.use_appearance_metadata,
-  use_camera_id=model_config.use_camera_metadata,
-  use_warp_id=model_config.use_warp,
-  random_seed=exp_config.random_seed,
+    exp_config.datasource_spec,
+    image_scale=exp_config.image_scale,
+    use_appearance_id=model_config.use_appearance_metadata,
+    use_camera_id=model_config.use_camera_metadata,
+    use_warp_id=model_config.use_warp,
+    random_seed=exp_config.random_seed,
 )
 
 # @title Initialize model
 # @markdown Defines the model and initializes its parameters.
 
-from flax.training import checkpoints
-from nerfies import models
-from nerfies import model_utils
-from nerfies import schedules
-from nerfies import training
 
 rng = random.PRNGKey(exp_config.random_seed)
 np.random.seed(exp_config.random_seed + jax.process_index())
@@ -107,21 +107,22 @@ devices = jax.devices()
 
 learning_rate_sched = schedules.from_config(train_config.lr_schedule)
 warp_alpha_sched = schedules.from_config(train_config.warp_alpha_schedule)
-elastic_loss_weight_sched = schedules.from_config(train_config.elastic_loss_weight_schedule)
+elastic_loss_weight_sched = schedules.from_config(
+    train_config.elastic_loss_weight_schedule)
 
 rng, key = random.split(rng)
 params = {}
 model, params['model'] = models.construct_nerf(
-  key,
-  model_config,
-  batch_size=train_config.batch_size,
-  appearance_ids=datasource.appearance_ids,
-  camera_ids=datasource.camera_ids,
-  warp_ids=datasource.warp_ids,
-  near=datasource.near,
-  far=datasource.far,
-  use_warp_jacobian=train_config.use_elastic_loss,
-  use_weights=train_config.use_elastic_loss,
+    key,
+    model_config,
+    batch_size=train_config.batch_size,
+    appearance_ids=datasource.appearance_ids,
+    camera_ids=datasource.camera_ids,
+    warp_ids=datasource.warp_ids,
+    near=datasource.near,
+    far=datasource.far,
+    use_warp_jacobian=train_config.use_elastic_loss,
+    use_weights=train_config.use_elastic_loss,
 )
 
 optimizer_def = optim.Adam(learning_rate_sched(0))
@@ -139,54 +140,53 @@ del params
 
 # @title Define pmapped render function.
 
-import functools
-from nerfies import evaluation
 
 def _model_fn(key_0, key_1, params, rays_dict, warp_extra):
-  out = model.apply(
-    { 'params': params },
-    rays_dict,
-    warp_extra=warp_extra,
-    rngs={
-      'coarse': key_0,
-      'fine': key_1
-    },
-    mutable=False,
-  )
-  return jax.lax.all_gather(out, axis_name='batch')
+    out = model.apply(
+        {'params': params},
+        rays_dict,
+        warp_extra=warp_extra,
+        rngs={
+            'coarse': key_0,
+            'fine': key_1
+        },
+        mutable=False,
+    )
+    return jax.lax.all_gather(out, axis_name='batch')
+
 
 pmodel_fn = jax.pmap(
-  # Note rng_keys are useless in eval mode since there's no randomness.
-  _model_fn,
-  in_axes=(0, 0, 0, 0, 0),  # Only distribute the data input.
-  devices=devices,
-  donate_argnums=(3,),  # Donate the 'rays' argument.
-  axis_name='batch',
+    # Note rng_keys are useless in eval mode since there's no randomness.
+    _model_fn,
+    in_axes=(0, 0, 0, 0, 0),  # Only distribute the data input.
+    devices=devices,
+    donate_argnums=(3,),  # Donate the 'rays' argument.
+    axis_name='batch',
 )
 
 # Make random seed separate across hosts.
 rng + jax.host_id()
 
-def render_fn(rays_dict):
-  _render_fn = functools.partial(
-    evaluation.render_image,
-    model_fn=pmodel_fn,
-    device_count=len(devices),
-    chunk=eval_config.chunk,
-  )
 
-  return _render_fn(state, rays_dict, rng=rng)
+def render_fn(rays_dict):
+    _render_fn = functools.partial(
+        evaluation.render_image,
+        model_fn=pmodel_fn,
+        device_count=len(devices),
+        chunk=eval_config.chunk,
+    )
+
+    return _render_fn(state, rays_dict, rng=rng)
 
 # @title Load cameras.
 
-from nerfies import utils
 
 print(f'Loading cameras from {camera_dir}')
 test_camera_paths = datasource.glob_cameras(camera_dir)
-test_cameras = utils.parallel_map(datasource.load_camera, test_camera_paths, show_pbar=True)
+test_cameras = utils.parallel_map(
+    datasource.load_camera, test_camera_paths, show_pbar=True)
 
 # @title Render video frames.
-from nerfies import visualization as viz
 
 rgb_imgs_dir = render_dir / 'rgb'
 depth_imgs_dir = render_dir / 'depth'
@@ -198,37 +198,39 @@ depth_imgs_dir.mkdir(exist_ok=True)
 
 results = []
 for i in range(len(test_cameras)):
-  rgb_img_path = rgb_imgs_dir / f"{i:05d}.png"
-  depth_img_path = depth_imgs_dir / f"{i:05d}.png"
-  if rgb_img_path.exists() and depth_img_path.exists():
-    print(f'Frame {i+1}/{len(test_cameras)} alread exists.  Skipping...')
-    continue
+    rgb_img_path = rgb_imgs_dir / f"{i:05d}.png"
+    depth_img_path = depth_imgs_dir / f"{i:05d}.png"
+    if rgb_img_path.exists() and depth_img_path.exists():
+        print(f'Frame {i+1}/{len(test_cameras)} alread exists.  Skipping...')
+        continue
 
-  print(f'Rendering frame {i + 1}/{len(test_cameras)}')
-  camera = test_cameras[i]
-  batch = datasets.camera_to_rays(camera)
-  batch['metadata'] = {
-    'appearance': jnp.zeros_like(batch['origins'][..., 0, jnp.newaxis], jnp.uint32),
-    'warp': jnp.zeros_like(batch['origins'][..., 0, jnp.newaxis], jnp.uint32),
-  }
+    print(f'Rendering frame {i + 1}/{len(test_cameras)}')
+    camera = test_cameras[i]
+    batch = datasets.camera_to_rays(camera)
+    batch['metadata'] = {
+        'appearance': jnp.zeros_like(batch['origins'][..., 0, jnp.newaxis], jnp.uint32),
+        'warp': jnp.zeros_like(batch['origins'][..., 0, jnp.newaxis], jnp.uint32),
+    }
 
-  output = render_fn(batch)
-  rgb = np.array(output['rgb'])
-  depth_med = np.array(output['med_depth'])
-  results.append((rgb, depth_med))
-  depth_viz = viz.colorize(depth_med.squeeze(), cmin=datasource.near, cmax=datasource.far, invert=True)
+    output = render_fn(batch)
+    rgb = np.array(output['rgb'])
+    depth_med = np.array(output['med_depth'])
+    results.append((rgb, depth_med))
+    depth_viz = viz.colorize(
+        depth_med.squeeze(), cmin=datasource.near, cmax=datasource.far, invert=True)
 
-  mediapy.write_image(rgb_img_path, rgb)
-  mediapy.write_image(depth_img_path, depth_viz)
+    mediapy.write_image(rgb_img_path, rgb)
+    mediapy.write_image(depth_img_path, depth_viz)
 
-  # @title Show rendered video.
+    # @title Show rendered video.
 
 fps = 30  # @param {type:'number'}
 
 frames = []
 for rgb, depth in results:
-  depth_viz = viz.colorize(depth.squeeze(), cmin=datasource.near, cmax=datasource.far, invert=True)
-  frame = np.concatenate([rgb, depth_viz], axis=1)
-  frames.append(image_utils.image_to_uint8(frame))
+    depth_viz = viz.colorize(
+        depth.squeeze(), cmin=datasource.near, cmax=datasource.far, invert=True)
+    frame = np.concatenate([rgb, depth_viz], axis=1)
+    frames.append(image_utils.image_to_uint8(frame))
 
 mediapy.write_video(render_dir / 'out.mp4', frames, fps=fps)
